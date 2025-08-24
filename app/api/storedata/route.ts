fc1731048ddb2190b9604382287dc84ddf0fdf3f
Gaurav <burandegaurav8899@gmail.com>
2025-08-24 12:41:47 +0530

    feat: send the hashed data to the blockchain too

--- app/api/storedata/route.ts
+++ app/api/storedata/route.ts
@@ -2,6 +2,8 @@ import { addDeviceReading } from "@/lib/redis";
 import { NextRequest, NextResponse } from "next/server";
 import { ethers } from "ethers";
 import abi from "@/app/abi.json";
+import { contractAddress } from "@/lib/utils";
+import { send } from "process";
 
 export async function POST(request: NextRequest) {
   try {
@@ -20,7 +22,8 @@ export async function POST(request: NextRequest) {
     console.log("Received data:", body);
     await addDeviceReading(body.device_id, body);
 
-    // todo: hash data and send it to the contract
+    await sendWeatherData(body);
+
     return NextResponse.json({
       message: "Data received and logged successfully",
       receivedData: body,
@@ -43,3 +46,41 @@ export async function GET() {
       "Send a POST request with Content-Type: application/json and a JSON body",
   });
 }
+
+export const sendWeatherData = async (weatherData: any) => {
+  try {
+    const { ethereum } = window as any;
+    if (!ethereum) throw new Error("MetaMask not found");
+
+    const provider = new ethers.BrowserProvider(ethereum);
+    const signer = await provider.getSigner();
+
+    const { chainId } = await provider.getNetwork();
+    if (chainId !== BigInt(43113)) {
+      await ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0xa869" }], // 43113 hex
+      });
+    }
+
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    const jsonString = JSON.stringify(weatherData);
+    const dataHash = ethers.keccak256(ethers.toUtf8Bytes(jsonString));
+
+    console.log("Weather JSON:", jsonString);
+    console.log("Hashed Data:", dataHash);
+
+    // Step 2: Call storeHash(deviceId, hash)
+    const tx = await contract.storeHash(weatherData.device_id, dataHash);
+    console.log("Tx sent:", tx.hash);
+
+    const receipt = await tx.wait();
+    console.log("Tx confirmed:", receipt);
+
+    alert("Weather data successfully stored on Avalanche Fuji!");
+  } catch (err) {
+    console.error("Error sending weather data:", err);
+    alert("Failed to send weather data");
+  }
+};
